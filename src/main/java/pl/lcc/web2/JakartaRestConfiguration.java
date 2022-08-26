bb8bce1793e774b777a0d8db1d2474afaba56e72
Teresa <Teresa@10.5.81.209>
2022-08-26 13:34:22 +0200

    firsts commit

---
+++ src/main/java/pl/lcc/web2/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package pl.lcc.web2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
